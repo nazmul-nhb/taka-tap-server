e7c9a6f8349275152875e16135e00c248b9fb748
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-16 03:06:49 +0600

    created register route with bcrypt hash

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,17 +1,32 @@
 import express from "express";
-import { userCollection } from "../db/takaDB.js";
+import bcrypt from "bcryptjs";
 import { ObjectId } from "mongodb";
+import { userCollection } from "../db/takaDB.js";
 
 const router = express.Router();
 
-// get users
-router.get('/', async (req, res) => {
+// register user
+router.post('/register', async (req, res) => {
     try {
-        const result = await userCollection.find().toArray();
-        res.send("Hello from Auth!");
+        const user = req.body;
+        const rawPIN = user.pin;
+
+        const userExists = await userCollection.findOne({ email: user.email });
+
+        // check for existing user
+        if (userExists) {
+            return res.status(409).send({ message: 'User Already Exists!' });
+        };
+
+        // generate hashed pin
+        const hashedPIN = await bcrypt.hash(rawPIN, 12);
+        user.pin = hashedPIN;
+
+        const result = await userCollection.insertOne(user);
+        res.send(result);
     } catch (error) {
-        console.error("Error Fetching Users:", error);
-        res.status(500).send({ message: "Internal Server Error!" });
+        console.error("Error Registering User: ", error);
+        res.status(500).send({ message: "Registration Error!" });
     }
 });
 
