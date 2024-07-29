947f7ae013e0ccd6a9ba20a8055a6e847918f304
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-29 21:36:01 +0600

    made send money route functional

--- routes/transactions.js
+++ routes/transactions.js
@@ -22,7 +22,7 @@ router.post('/request', verifyToken, async (req, res) => {
         const transInfo = req.body;
 
         // check if user has enough balance for cash out request
-        if (transInfo.request_type === "cash-out") {
+        if (transInfo.transaction_type === "cash-out") {
             const payableAmount = transInfo.amount * 1.015
 
             const isEnoughBalance = await userCollection.findOne({ email, balance: { $gte: payableAmount } });
@@ -65,7 +65,61 @@ router.post('/send', verifyToken, async (req, res) => {
 
         const transInfo = req.body;
 
-        console.log(transInfo, req.user);
+        let payableAmount = transInfo.amount;
+
+        if (payableAmount) {
+            if (transInfo.amount > 100) {
+                payableAmount = transInfo.amount + 5;
+            }
+            // return if send money amount is below 50 taka
+            if (payableAmount < 50) {
+                return res.send({ success: false, message: 'Send at least 50 taka!' });
+            }
+
+            // check if user has enough balance for cash out request
+            const isEnoughBalance = await userCollection.findOne({ email, balance: { $gte: payableAmount } });
+
+            if (!isEnoughBalance) {
+                return res.send({ success: false, message: 'Insufficient Balance!' });
+            }
+
+            // check if the receiver number user provided is not his own number
+            if (mobile === transInfo.receiver) {
+                return res.send({ success: false, message: 'Cannot send money to your own account!' });
+            }
+            // check if the receiver number user provided is valid and not an agent
+            const receiverExists = await userCollection.findOne({ mobile: transInfo.receiver, account_type: "user" });
+            if (!receiverExists) {
+                return res.send({ success: false, message: 'Provide A Valid Receiver Number!' });
+            }
+
+            const options = { returnDocument: 'after' };
+
+            // update balance in sender and receiver profiles and get updated info
+            const sender = await userCollection.findOneAndUpdate({ mobile }, { $inc: { balance: -payableAmount } }, options);
+            const receiver = await userCollection.findOneAndUpdate({ mobile: transInfo.receiver }, { $inc: { balance: transInfo.amount } }, options);
+
+            const transactionID = generateTransactionID();
+
+            const senderData = {
+                name, mobile, email, balance: sender.balance, transactionID, ...transInfo
+            };
+
+            delete transInfo.receiver;
+
+            const receiverData = {
+                name: receiver.name, mobile: receiver.mobile, email: receiver.email, sender: mobile, transactionID, ...transInfo,
+            };
+
+            // add in transaction collection
+            const transactionResult = await transactionCollection.insertMany([senderData, receiverData]);
+
+            if (transactionResult.insertedIds) {
+                return res.status(201).send({ success: true, message: 'Money Sent!' });
+            } else {
+                return res.status(500).send({ success: false, message: 'Internal Server Error!' });
+            }
+        }
     } catch (error) {
         console.error("Send Money Error: ", error);
         res.status(500).send({ message: 'Internal Server Error!' });
